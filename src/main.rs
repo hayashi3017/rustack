7049003dd84031fba08bce7195257dc92e94682d
hayashi3017 <kuro.love.3017ch@gmail.com>
2024-08-09 22:27:21 +0900

    2.4 パースとコマンドの実行

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,48 @@
 fn main() {
-    let mut stack = vec![];
+    for line in std::io::stdin().lines() {
+        let mut stack = vec![];
 
-    stack.push(42);
-    stack.push(36);
+        if let Ok(line) = line {
+            let words: Vec<_> = line.split(" ").collect();
 
-    add(&mut stack);
-
-    stack.push(22);
-
-    add(&mut stack);
-
-    println!("stack: {stack:?}");
+            for word in words {
+                if let Ok(parsed) = word.parse::<i32>() {
+                    stack.push(parsed);
+                } else {
+                    match word {
+                        "+" => add(&mut stack),
+                        "-" => sub(&mut stack),
+                        "*" => mul(&mut stack),
+                        "/" => div(&mut stack),
+                        _ => panic!("{word:?} could not be parsed"),
+                    }
+                }
+            }
+            println!("stack: {stack:?}");
+        }
+    }
 }
 
 fn add(stack: &mut Vec<i32>) {
     let lhs = stack.pop().unwrap();
     let rhs = stack.pop().unwrap();
     stack.push(lhs + rhs);
+}
+
+fn sub(stack: &mut Vec<i32>) {
+    let lhs = stack.pop().unwrap();
+    let rhs = stack.pop().unwrap();
+    stack.push(lhs - rhs);
+}
+
+fn mul(stack: &mut Vec<i32>) {
+    let lhs = stack.pop().unwrap();
+    let rhs = stack.pop().unwrap();
+    stack.push(lhs * rhs);
+}
+
+fn div(stack: &mut Vec<i32>) {
+    let lhs = stack.pop().unwrap();
+    let rhs = stack.pop().unwrap();
+    stack.push(lhs / rhs);
 }
