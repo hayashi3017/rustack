14f42965108c693c5dd37c02a0d648e642211a09
hayashi3017 <kuro.love.3017ch@gmail.com>
2024-08-11 10:56:03 +0900

    2.7 変数の定義

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,36 @@
+use std::collections::HashMap;
+
 fn main() {
     for line in std::io::stdin().lines().flatten() {
         parse(&line);
     }
 }
 
-fn eval<'src>(code: Value<'src>, stack: &mut Vec<Value<'src>>) {
+fn eval<'src>(code: Value<'src>, vm: &mut Vm<'src>) {
     match code {
         Value::Op(op) => match op {
-            "+" => add(stack),
-            "-" => sub(stack),
-            "*" => mul(stack),
-            "/" => div(stack),
-            "if" => op_if(stack),
-            _ => panic!("{op:?} could not be parsed"),
+            "+" => add(&mut vm.stack),
+            "-" => sub(&mut vm.stack),
+            "*" => mul(&mut vm.stack),
+            "/" => div(&mut vm.stack),
+            "<" => lt(&mut vm.stack),
+            "if" => op_if(vm),
+            "def" => op_def(vm),
+            _ => {
+                let val = vm
+                    .vars
+                    .get(op)
+                    .expect(&format!("{op:?} is not a define operation"));
+                vm.stack.push(val.clone());
+                // vm.stack.push(*val); でもよい？
+            }
         },
-        _ => stack.push(code.clone()),
+        _ => vm.stack.push(code.clone()),
     }
 }
 
 fn parse<'a>(line: &'a str) -> Vec<Value> {
-    let mut stack = vec![];
+    let mut vm = Vm::new();
     let input: Vec<_> = line.split(" ").collect();
     let mut words = &input[..];
     while let Some((&word, mut rest)) = words.split_first() {
@@ -29,20 +40,22 @@ fn parse<'a>(line: &'a str) -> Vec<Value> {
         if word == "{" {
             let value;
             (value, rest) = parse_block(rest);
-            stack.push(value);
+            vm.stack.push(value);
         } else {
             let code = if let Ok(num) = word.parse::<i32>() {
                 Value::Num(num)
+            } else if word.starts_with("/") {
+                Value::Sym(&word[1..]) // 先頭の`/`を除外する
             } else {
                 Value::Op(word)
             };
-            eval(code, &mut stack);
+            eval(code, &mut vm);
         }
 
         words = rest;
     }
-    println!("stack: {stack:?}");
-    stack
+    println!("stack: {:?}", vm.stack);
+    vm.stack
 }
 
 fn parse_block<'src, 'a>(input: &'a [&'src str]) -> (Value<'src>, &'a [&'src str]) {
@@ -72,55 +85,86 @@ fn parse_block<'src, 'a>(input: &'a [&'src str]) -> (Value<'src>, &'a [&'src str
 }
 
 fn add(stack: &mut Vec<Value>) {
-    let lhs = stack.pop().unwrap().as_num();
     let rhs = stack.pop().unwrap().as_num();
+    let lhs = stack.pop().unwrap().as_num();
     stack.push(Value::Num(lhs + rhs));
 }
 
 fn sub(stack: &mut Vec<Value>) {
-    let lhs = stack.pop().unwrap().as_num();
     let rhs = stack.pop().unwrap().as_num();
+    let lhs = stack.pop().unwrap().as_num();
     stack.push(Value::Num(lhs - rhs));
 }
 
 fn mul(stack: &mut Vec<Value>) {
-    let lhs = stack.pop().unwrap().as_num();
     let rhs = stack.pop().unwrap().as_num();
+    let lhs = stack.pop().unwrap().as_num();
     stack.push(Value::Num(lhs * rhs));
 }
 
 fn div(stack: &mut Vec<Value>) {
-    let lhs = stack.pop().unwrap().as_num();
     let rhs = stack.pop().unwrap().as_num();
+    let lhs = stack.pop().unwrap().as_num();
     stack.push(Value::Num(lhs / rhs));
 }
 
-fn op_if(stack: &mut Vec<Value>) {
-    let false_branch = stack.pop().unwrap().to_block();
-    let true_branch = stack.pop().unwrap().to_block();
-    let cond = stack.pop().unwrap().to_block();
+fn lt(stack: &mut Vec<Value>) {
+    let rhs = stack.pop().unwrap().as_num();
+    let lhs = stack.pop().unwrap().as_num();
+    // trueが1、falseが0 としてキャストされる
+    stack.push(Value::Num((lhs < rhs) as i32));
+}
+
+fn op_if<'src>(vm: &mut Vm<'src>) {
+    let false_branch = vm.stack.pop().unwrap().to_block();
+    let true_branch = vm.stack.pop().unwrap().to_block();
+    let cond = vm.stack.pop().unwrap().to_block();
 
     for code in cond {
-        eval(code, stack);
+        eval(code, vm);
     }
 
-    let cond_result = stack.pop().unwrap().as_num();
+    let cond_result = vm.stack.pop().unwrap().as_num();
 
     if cond_result != 0 {
         for code in true_branch {
-            eval(code, stack);
+            eval(code, vm);
         }
     } else {
         for code in false_branch {
-            eval(code, stack);
+            eval(code, vm);
         }
     }
 }
 
+fn op_def(vm: &mut Vm) {
+    let value = vm.stack.pop().unwrap();
+    eval(value, vm);
+    let value = vm.stack.pop().unwrap();
+    let sym = vm.stack.pop().unwrap().as_sym();
+
+    vm.vars.insert(sym, value);
+}
+
+// stackに加えvarsという状態を扱いたいが、引数に複数の状態を持たせるのが煩雑　→　Vmという状態にまとめる
+struct Vm<'src> {
+    stack: Vec<Value<'src>>,
+    vars: HashMap<&'src str, Value<'src>>,
+}
+
+impl<'src> Vm<'src> {
+    fn new() -> Self {
+        Self {
+            stack: vec![],
+            vars: HashMap::new(),
+        }
+    }
+}
 #[derive(Debug, PartialEq, Eq, Clone)]
 enum Value<'src> {
     Num(i32),
     Op(&'src str),
+    Sym(&'src str),
     Block(Vec<Value<'src>>),
 }
 
@@ -129,6 +173,14 @@ impl<'src> Value<'src> {
         match self {
             Self::Num(val) => *val,
             _ => panic!("Value is not a number"),
+        }
+    }
+
+    fn as_sym(&self) -> &'src str {
+        if let Self::Sym(sym) = self {
+            *sym
+        } else {
+            panic!("Value is not a symbol");
         }
     }
 
@@ -159,5 +211,23 @@ mod test {
     #[test]
     fn test_if_true() {
         assert_eq!(parse("{ 1 1 + } { 100 } { -100 } if"), vec![Num(100)])
+    }
+
+    #[test]
+    fn test_var_unit() {
+        assert_eq!(parse("/x 10 def x 1 +"), vec![Num(11)])
+    }
+
+    #[test]
+    fn test_var() {
+        assert_eq!(parse("/x 10 def /y 20 def x y *"), vec![Num(200)])
+    }
+
+    #[test]
+    fn test_var_if() {
+        assert_eq!(
+            parse("/x 10 def /y 20 def { x y < } { x } { y } if"),
+            vec![Num(10)]
+        );
     }
 }
